5e2facca8c2344bd2a0685e9441a42b9d5a59361
pachacamac <marcanbe@gmail.com>
2019-03-08 02:58:44 +0100

    lots of improvements and auto-reloading

--- README.md
+++ README.md
@@ -29,23 +29,36 @@ Or install it yourself as:
 
 call `jing` on the command line or have a look at the code (it's tiny) :)
 
-
-Creates a new folder `mypage` with a basic setup:
+Creates a new folder `mypage` or updates an existing one and adds a basic setup:
 
     $ jing create mypage
     $ cd mypage
-    
+
 Build the current project:
-    
+
     $ jing build
-    
+
 Automatically build the page whenever a file in the project folder changes:
 
     $ jing watch
-    
-Serves the current project folder on `http://0.0.0.0:8000`:
-    
+    $ jing watch -full_build true   # to not skip uglifying js files (slower)
+
+Serves the current projects _dst folder on `http://0.0.0.0:8000`:
+
     $ jing serve
+    $ jing serve -no_auto_reload true       # to not inject auto page reload code into html files
+    $ jing serve -port 1234 -root somepath  # to change port and root directory
+
+Options that work in all commands:
+    -src somepath     # changes the source folder - default: ./ (current folder)
+    -dst somepath     # changes the output folder - default: ./_dst
+    -layouts folder   # changes the layouts folder - default: ./_layouts
+    -partials folder  # changes the partials folder - default: ./_partials
+
+
+Show current version
+
+    $ jing version
 
 File endings work like Russian dolls: They get converted from outer to inner. Rails folks should be somewhat familiar with that but here it's a bit stricter.
 
@@ -55,6 +68,8 @@ Folders starting with `_` have special meaning, they generally won't get copied 
 
 `_layouts` holds layouts (TODO: explanation)
 
+`_dst' holds the generated site
+
 `.meta.yml` holds global meta variables available in templates when not overwritten
 
 ~~I'll try and add a basic example project soon.~~ Check out the examples folder. If you feel like giving this a try and have questions feel free to open an issue or reach out otherwise. Pull requests welcome too.
@@ -63,7 +78,7 @@ Folders starting with `_` have special meaning, they generally won't get copied 
 
 Extending is fairly easy.
 
-`@converters` variable in the initialize method holds a bunch of converters. Just add one in the spirit of the others. You can also do this on the fly when initiating the Jing Class by passing the `converter_addons` option.
+`@converters` variable in the initialize method holds a bunch of converters. Just add one in the spirit of the others.
 
 Class methods that end in a bang `!` automatically are registered as cli commands. If you wish to add a command just add a bang-method and you should be done.
 
--- lib/jing.rb
+++ lib/jing.rb
@@ -1,8 +1,29 @@
-require "jing/version"
+%w[jing/version erb yaml fileutils time webrick bundler/inline].each { |e| require e }
+gemfile { source('https://rubygems.org')
+  %w[filewatcher sassc typescript-node uglifier kramdown].each { |e| gem e }
+}
 
-%w[erb yaml fileutils time webrick ostruct bundler/inline].each { |e| require e }
+module SymbolizeHelper
+  extend self
+  def symbolize_recursive(hash)
+    {}.tap{ |h| hash.each { |key, value| h[key.to_sym] = transform(value) } }
+  end
+  private
+  def transform(thing)
+    return symbolize_recursive(thing) if thing.is_a?(Hash)
+    return thing.map { |v| transform(v) } if thing.is_a?(Array)
+    thing
+  end
+  refine Hash do
+    def deep_symbolize_keys
+      SymbolizeHelper.symbolize_recursive(self)
+    end
+  end
+end
 
 module Jing
+  using SymbolizeHelper
+
   class Jing
     attr_accessor :converters, :converter_extensions
 
@@ -11,44 +32,36 @@ module Jing
       @dst = File.expand_path(opts[:dst] || File.join(@src, '_dst'))
       @layouts = opts[:layouts] || '_layouts'
       @partials = opts[:partials] || '_partials'
-      @loaded_gems = {}
-      @converters = (opts[:converter_addons] || []) + [
-        {extensions: %w[erb], handler: Proc.new { |body, meta, ctx|
-          ERB.new(body).result(OpenStruct.new(meta: meta).instance_eval { ctx })
-        }},
-        {extensions: %w[html htm], handler: Proc.new { |body, meta, ctx|
+      @converters = {
+        %w[erb] => ->(body, meta, ctx){
+          ctx.local_variable_set(:meta, meta)
+          ERB.new(body).result(ctx)
+        },
+        %w[html htm] => ->(body, meta, ctx){
+          ctx.local_variable_set(:meta, meta)
           if meta[:layout] && layout = Dir[File.join(@src, @layouts, "#{meta[:layout]}.*")].first
-            content = load_content(layout, meta)
-            ERB.new(content[:body]).result(OpenStruct.new(meta: meta, body: body).instance_eval { ctx })
+            ctx.local_variable_set(:body, body)
+            ERB.new(load_content(layout, meta)[:body]).result(ctx)
           else
-            ERB.new(body).result(OpenStruct.new(meta: meta).instance_eval { ctx })
+            ERB.new(body).result(ctx)
           end
-        }},
-        {extensions: %w[scss sass css], handler: ->(body, meta, ctx){
-          load_gem 'sassc'
+        },
+        %w[scss sass css] => ->(body, meta, ctx){
           SassC::Engine.new(body, style: (meta[:style] || :compressed)).render
-        }},
-        {extensions: %w[ts], handler: ->(body, meta, ctx){
-          load_gem 'typescript-node'
+        },
+        %w[ts] =>->(body, meta, ctx){
           TypeScript::Node.compile(body, '--target', (meta[:style] || 'ES5'))
-        }},
-        {extensions: %w[js], handler: ->(body, meta, ctx){
-          load_gem 'uglifier'
+        },
+        %w[js] => ->(body, meta, ctx){
           Uglifier.compile(body, harmony: true, compress: false, mangle: false, output: {ascii_only: true})
-        }},
-        {extensions: %w[md markdown], handler: ->(body, meta, ctx){
-          load_gem 'kramdown'
+        },
+        %w[md markdown] => ->(body, meta, ctx){
           Kramdown::Document.new(body).to_html
-        }},
-      ]
+        }
+      }
 
-      @converter_extensions = @converters.map { |e| e[:extensions] }.flatten
-      main_meta_file = File.join(@src, '.meta.yml')
-      @meta = YAML.load(File.read(main_meta_file)).map { |k,v| [k.to_sym,v] }.to_h if File.exist?(main_meta_file)
-    end
-
-    def load_gem(name, opts={})
-      @loaded_gems[name] ||= gemfile { source(opts.delete(:source)||'https://rubygems.org'); gem(name, opts) }
+      @converter_extensions = @converters.keys.flatten
+      File.join(@src, '.meta.yml').tap{|e| @meta = File.exist?(e) ? YAML.load(File.read(e)).deep_symbolize_keys : {}}
     end
 
     def active_exts(file)
@@ -62,15 +75,15 @@ module Jing
     end
 
     def load_content(file, meta={})
-      body = File.open(file, 'rb').read
-      return {body: body, meta: meta} unless @converter_extensions.include?(File.extname(file)[1..-1])
+      body = File.open(file, 'rb'){|f|f.read}
+      return {body: body, meta: meta.deep_symbolize_keys} unless @converter_extensions.include?(File.extname(file)[1..-1])
       body.match(/^(?:(---\s*\n.*?\n?)^(?:---\s*$\n?))?(.*)$/m)
       meta.merge!(YAML.load($1).map { |k,v| [k.to_sym,v] }.to_h) if $1
-      {body: $2, meta: meta}
+      {body: $2, meta: meta.deep_symbolize_keys}
     end
 
     def render(file, meta={})
-      if !File.file?(file)
+      unless File.file?(file)
         file = Dir[File.join(@src, @partials, "#{file}.*")].first
       else
         meta.merge!(file: file)
@@ -80,19 +93,19 @@ module Jing
       body = content[:body]
       exts = active_exts(file)
       exts.each do |ext|
-        converter = @converters.find { |c| c[:extensions].include?(ext) }
-        body = converter ? converter[:handler].call(body, content[:meta], binding) : body
+        converter = @converters[@converters.keys.find { |k| k.include?(ext) }]
+        body = converter ? converter.call(body, content[:meta], binding) : body
       end
-      puts "#{'%.4fs' % (Time.now-t)}\t#{file[@src.size+1..-1]} >#{exts.join('>')}> #{dstname(file, @dst)[@dst.size+1..-1]} (#{(body.size/1024.0).round(2)}kb)"
+      puts "#{'%.4fs' % (Time.now-t)}\t#{file[@src.size+1..-1]} >#{exts.join('>')}> #{dstname(file, @dst)[@dst.size+1..-1]} (#{ '%.2fkb' % (body.size/1024.0)})"
       body
     rescue => e
       puts "Error\t#{file[@src.size+1..-1]}\n\t#{e.message}\n#{e.backtrace.map{|x| "\t#{x}"}.join("\n")}"
     end
 
     def build!(opts={})
-      t = Time.now
-      FileUtils.rm_r(@dst) if File.exist?(@dst)
-      Dir.mkdir(@dst)
+      t,s = Time.now, 0
+      FileUtils.rm_rf("#{@dst}/.", secure: true)
+      Dir.mkdir(@dst) unless File.exist?(@dst)
       Dir[File.join(@src, '**', '*')].each do |file|
         next unless File.file?(file)
         dir = File.dirname(file)[@src.size+1..-1]
@@ -100,18 +113,16 @@ module Jing
         outfile = dstname(file, File.join(*[@dst, dir].compact))
         out = render(file, @meta.merge(layout: dir))
         FileUtils.mkdir_p(File.dirname(outfile))
-        File.open(outfile, 'wb').write(out)
+        File.open(outfile, 'wb'){|f|s+=f.write(out)}
       end
-      puts "#{'%.4fs' % (Time.now-t)} total"
+      puts "#{'%.4fs' % (Time.now-t)}, #{'%.2fkb' % (s/1024.0)} total"
     end
 
     def watch!(opts={})
-      @converters.delete_if{|e| e[:extensions]==['js']}
-      @converters<<{extensions: %w[js], handler: ->(body, meta, ctx){body}}
+      @converters[%w[js]] = ->(body, meta, ctx){puts 'skipping uglyfier'; body} unless opts[:full_build]
       build!(opts)
-      load_gem('filewatcher')
       Filewatcher.new([@src, '**', '*']).watch do |filename, event|
-        unless File.expand_path(filename) == @dst
+        unless filename.start_with?(@dst)
           puts "\nWATCHED: #{filename}\t#{event}\t#{Time.now}"
           build!(opts)
         end
@@ -119,13 +130,39 @@ module Jing
     end
 
     def serve!(opts={})
-      WEBrick::HTTPServer.new(Port: opts[:port] || 8000, DocumentRoot: opts[:root] || @dst).start
+      api_route = opts[:api_route] || '/_J_I_N_G_'
+      inter = opts[:interval] || 2000
+      script = "<script>((t)=>{setInterval(()=>{fetch('#{api_route}').then(r=>r.json()).then((j)=>{if(Date.parse(j.modified)>t){fetch(document.location.pathname).then((r)=>{if(r.ok)window.location.reload(true)})}})}, #{inter})})(new Date().getTime())</script>"
+      srv = WEBrick::HTTPServer.new(Port: opts[:port] || 8000, DocumentRoot: opts[:root] || @dst)
+
+      srv.mount_proc('/') do |rq, rs|
+        path = File.join(@dst, rq.path == '/' ? '/index.html' : rq.path)
+        raise WEBrick::HTTPStatus::NotFound, "`#{rq.path}' not found." unless File.exist?(path)
+        st = File::stat(path)
+        rs['etag'] = sprintf("%x-%x-%x", st.ino, st.size, st.mtime.to_i)
+        rs['content-type'] = WEBrick::HTTPUtils::mime_type(path, WEBrick::HTTPUtils::DefaultMimeTypes)
+        rs['last-modified'] = st.mtime.httpdate
+        if rs['content-type'] == 'text/html'
+          rs.body = File.open(path, "rb").read.gsub(/(<\/body>)/im){"\n#{script}\n#{$1}"}
+          rs['content-length'] = rs.body.size.to_s
+        else
+          rs['content-length'] = st.size.to_s
+          rs.body = File.open(path, "rb")
+        end
+      end unless opts[:no_auto_reload]
+
+      srv.mount_proc(api_route) do |req, res|
+        res['Content-Type'] = 'application/json'
+        res.body = {modified: File.stat(@dst).ctime.iso8601}.to_json
+      end
+      trap('INT'){ srv.stop }
+      srv.start
     end
 
     def create!(opts={})
       abort("usage: #{File.basename($0)} create -name <pathname>") unless opts[:name]
       [@layouts, @partials].each { |e| FileUtils.mkdir_p(File.join(opts[:name], e)) }
-      File.write(File.join(opts[:name], '.meta.yml'), "---\ngenerator: jing #{VERSION}\nname: #{File.basename(opts[:name])}\n---\n")
+      File.write(File.join(opts[:name], '.meta.yml'), "---\ngenerator: jing\nname: #{File.basename(opts[:name])}\n---\n")
     end
 
     def version!(opts={})
